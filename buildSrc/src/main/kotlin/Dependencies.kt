b27b27f7a2d8cbdb6f6dff5992a51564f8f3bfc0
alexoooo <alex@ostrovsky.biz>
2021-03-25 16:44:41 -0400

    dependency update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -11,4 +11,4 @@ const val immutaleJsVersion = "3.8.2"
 const val dexxVersion = "0.7"
 const val coreJsVersion = "3.6.5"
 
-const val guavaVersion = "30.1-jre"
+const val guavaVersion = "30.1.1-jre"
