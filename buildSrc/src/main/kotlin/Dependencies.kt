079ade72a9fe7528cc04f322f36341776b5e856a
alexoooo <alex@ostrovsky.biz>
2022-03-11 23:20:16 -0500

    gradle upgrade, yarn lock file

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,10 +4,11 @@ const val jvmTargetVersion = "17"
 const val javaVersion = 17
 const val jvmToolchainVersion = 17
 
-const val coroutinesVersion = "1.5.2"
+const val coroutinesVersion = "1.6.0"
 
-const val guavaVersion = "31.0.1-jre"
+const val guavaVersion = "31.1-jre"
 const val dexxVersion = "0.7"
 
-const val coreJsVersion = "3.19.3"
-const val immutaleJsVersion = "3.8.2"
+const val coreJsVersion = "3.21.1"
+//const val immutaleJsVersion = "3.8.2"
+const val immutaleJsVersion = "4.0.0"
