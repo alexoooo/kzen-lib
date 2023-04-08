0dc698b63cef79d4444514dfc5726e02dd3fb15d
alexoooo <alex@ostrovsky.biz>
2023-04-08 10:14:32 -0400

    dependency update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,4 +1,4 @@
-const val kotlinVersion = "1.8.10"
+const val kotlinVersion = "1.8.20"
 const val jvmTargetVersion = "19"
 const val javaVersion = 19
 const val jvmToolchainVersion = 19
@@ -8,5 +8,5 @@ const val coroutinesVersion = "1.6.4"
 const val guavaVersion = "31.1-jre"
 const val dexxVersion = "0.7"
 
-const val coreJsVersion = "3.29.0"
-const val immutaleJsVersion = "4.2.3"
+const val coreJsVersion = "3.30.0"
+const val immutaleJsVersion = "4.3.0"
