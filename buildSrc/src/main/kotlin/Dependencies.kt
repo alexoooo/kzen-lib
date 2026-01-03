947c11368ff58828d1e2bc2dc8a5252692ceaa4f
alexoooo <alex@ostrovsky.biz>
2026-01-02 21:43:37 -0500

    dependency update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,15 +1,15 @@
 // Info about which JDK each Kotlin release is compatible with:
 //  https://stackoverflow.com/a/77179226/1941359
 
-const val kotlinVersion = "2.0.0"
-const val jvmTargetVersion = "21"
-const val javaVersion = 21
-const val jvmToolchainVersion = 21
+const val kotlinVersion = "2.3.0"
+const val jvmTargetVersion = "25"
+const val javaVersion = 25
+const val jvmToolchainVersion = 25
 
-const val coroutinesVersion = "1.8.1"
+const val coroutinesVersion = "1.10.2"
 
-const val guavaVersion = "33.2.1-jre"
+const val guavaVersion = "33.5.0-jre"
 const val dexxVersion = "0.7"
 
-const val coreJsVersion = "3.37.1"
-const val immutableJsVersion = "4.3.6"
+const val coreJsVersion = "3.47.0"
+const val immutableJsVersion = "5.1.4"
