531c2c3d00a2a4e5f4124805cccd729c26a8505e
alexoooo <alex@ostrovsky.biz>
2026-05-10 13:47:18 -0400

    server-side dependency updates

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,9 +6,9 @@ const val jvmTargetVersion = "25"
 const val javaVersion = 25
 const val jvmToolchainVersion = 25
 
-const val coroutinesVersion = "1.10.2"
+const val coroutinesVersion = "1.11.0"
 
-const val guavaVersion = "33.5.0-jre"
+const val guavaVersion = "33.6.0-jre"
 const val dexxVersion = "0.7"
 
 const val coreJsVersion = "3.47.0"
